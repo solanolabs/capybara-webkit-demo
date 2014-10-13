ef010782f519078c60f551b311f0cb5b643ea24b
Jay Moorthi <jay@tddium.com>
2014-10-12 21:20:21 -0700

    No sudo

--- startHeadlessBrowser.sh
+++ startHeadlessBrowser.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "installing xorg dummy driver"
-sudo apt-get install -y xserver-xorg-video-dummy
+#echo "installing xorg dummy driver"
+#sudo apt-get install -y xserver-xorg-video-dummy
 echo "Starting Xorg server"
 cmd="/usr/bin/Xorg -noreset +extension GLX +extension RANDR +extension RENDER -logfile tmp/xorg.log  -config xorg.conf -novtswitch  $1"
 echo "command: $cmd"
